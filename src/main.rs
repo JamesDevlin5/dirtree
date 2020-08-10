cde817133001f5f401756555df0456b2827c7c38
James Devlin <devlin.ja@northeastern.edu>
2020-08-10 07:43:01 -0400

    Created Counter struct and associated functions
    Created the Counter struct to maintain a count of the files and
    directories traversed. The fields of this struct are initialized to
    zero. The display function for this struct outputs a terminal friendly
    summary of directories and files traversed.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
+mod counter {
+    use std::fmt;
+    struct Counter {
+        dirs: usize,
+        files: usize,
+    }
+    impl Counter {
+        fn new() -> Self {
+            Counter { dirs: 0, files: 0 }
+        }
+    }
+    impl fmt::Display for Counter {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "{} directories, {} files", self.dirs, self.files)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_new() {
+            let c = Counter::new();
+            assert_eq!(c.dirs, 0);
+            assert_eq!(c.files, 0);
+        }
+
+        #[test]
+        fn test_display() {
+            assert_eq!(format!("{}", Counter::new()), "0 directories, 0 files");
+            assert_eq!(
+                format!(
+                    "{}",
+                    Counter {
+                        dirs: 10,
+                        files: 10
+                    }
+                ),
+                "10 directories, 10 files"
+            );
+            assert_eq!(
+                format!(
+                    "{}",
+                    Counter {
+                        dirs: 100,
+                        files: 1
+                    }
+                ),
+                "100 directories, 1 files"
+            );
+            assert_eq!(
+                format!(
+                    "{}",
+                    Counter {
+                        dirs: 512,
+                        files: 1024
+                    }
+                ),
+                "512 directories, 1024 files"
+            );
+            assert_eq!(
+                format!("{}", Counter { dirs: 0, files: 1 }),
+                "0 directories, 1 files"
+            );
+        }
+    }
+}
 fn main() {
     println!("Hello, world!");
 }
